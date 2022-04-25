3c1d17be43bbe446ee6bbf39c93ee93c024aa7ad
oguzhandeniz <95766710+oguzhandeniz@users.noreply.github.com>
2022-04-25 22:03:21 +0300

    Update footer.blade.php

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="pd-ltr-20 xs-pd-20-10">
         <div class="min-height-200px">
             <div class="footer-wrap pd-20 mb-20 card-box">
-                DeskApp - Desing By <a href="https://github.com/oguzhandeniz" target="_blank">Oğuzhan DENİZ</a>
+                DeskApp - Desing By <a href="https://github.com/oguzhandeniz" target="_blank">OğuzhanDENİZ</a>
             </div>
         </div>
     </div>
